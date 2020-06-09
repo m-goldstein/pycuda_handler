4c40985d1c9355ded3f3e54ba143a9b9836a8688
maximilian <m-goldstein@github.com>
2020-06-09 22:32:15 +0000

    rework

--- lib/launcher.py
+++ lib/launcher.py
@@ -30,7 +30,29 @@ def get_kernel(file_name=None, kern_name=None):
     except:
 	    print("[get_kernel] Error: could not extract function pointer.\n")
 	    return None
-	
+
+def kernel_launcher(kernel=None, input_data=None, output_data=None):
+    if kernel is None:
+	    print("[kernel_launcher] Error: No kernel selected.\n")
+	    return None
+    if input_data is None:
+	    print("[kernel_launcher] Error: No input data specified.\n")
+	    return None
+    try:
+	    input_size          = numpy.int32(len(input_data))
+	    print("Input Vector: ")
+	    print(input_data)
+	    output_size         = input_size / (BLOCK_SIZE << 1)
+	    if (input_size % (BLOCK_SIZE << 1)) != 0:
+		    output_size += 1
+	    blockDim            = (BLOCK_SIZE, 1, 1)
+	    gridDim             = (math.ceil(output_size / (1.0 * TILE_SIZE)) * TILE_SIZE, 1, 1) 
+	    kernel(drv.In(input_data), drv.Out(output_data), input_size, block=blockDim, grid=gridDim)
+	    return output_data
+    except:
+	    print("[kernel_launcher] Error: could not execute kernel.\n")
+	    return None
+
 kernel          = get_kernel('avg_kernel.cu', 'avg_kernel')
 client          = init_client()
 results         = client.make_query("execution_time", True, 100000)
@@ -38,19 +60,12 @@ exchange_vector = vectorize_by_exchange(results)
 
 key_vector = [key for key in exchange_vector.keys()]
 for key in key_vector:
-	data_in  = exchange_vector[key]
-	data_in  = numpy.asarray(data_in).astype(numpy.float32)
-	data_out = numpy.zeros_like(data_in)
+	data_in        = exchange_vector[key]
+	data_in        = numpy.asarray(data_in).astype(numpy.float32)
+	data_out       = numpy.zeros_like(data_in)
 	input_size     = numpy.int32(len(data_in))
 	print("Results for exchange %s"%(key))
-	print("Input Vector: ")
-	print(data_in)
-	output_size = input_size / (BLOCK_SIZE << 1)
-	if (input_size % (BLOCK_SIZE << 1)) != 0:
-		output_size += 1
-	blockDim = (BLOCK_SIZE, 1, 1)
-	gridDim  = (math.ceil(output_size / (1.0 * TILE_SIZE)) * TILE_SIZE, 1, 1) 
-	kernel(drv.In(data_in), drv.Out(data_out), input_size, block=blockDim, grid=gridDim)
+	output = kernel_launcher(kernel, data_in, data_out)
 	print("Output Vector: ")
-	print(data_out[0])
+	print(output[0])
 print("\n")
